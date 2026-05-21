d40edf85ce4cc870a35198b8e922d2cdf26439b0
ducdatdau <contact.ducdatdau@gmail.com>
2026-05-21 19:45:27 +0700

    add screenshot.sh file

---
+++ afl-2.57b/analyze/screenshot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SAVE_DIR="$HOME/MOD-AFLGO/analyze/results"
+
+while true; do
+    TIME_STAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+    FILE_PATH="$SAVE_DIR/screenshot_$TIME_STAMP.png"
+
+    gnome-screenshot -f "$FILE_PATH"
+
+    echo "[$(date +"%H:%M:%S")] Saved: $FILE_PATH"
+
+    sleep 300
+done
